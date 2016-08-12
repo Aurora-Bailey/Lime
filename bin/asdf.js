8f2aba86826dea9d38f6c4947d646dfc522ad764
Brad Thompson <bradleyglind@gmail.com>
2016-08-12 09:34:37 -0600

    multithread testing

---
+++ bin/asdf.js
@@ -0,0 +1,153 @@
+
+// On both the client and server:
+var sp = require('schemapack');
+
+var playerSchema = sp.build({p: [{
+    id: 'varuint',
+    transform: {
+        position: {
+            x: 'float64',
+            y: 'float64',
+            z: 'float64'
+        },
+        rotation: {
+            x: 'float64',
+            y: 'float64',
+            z: 'float64',
+            w: 'float64'
+        }
+    }
+}, {
+    id: 'varuint',
+    transform: {
+        position: {
+            x: 'float64',
+            y: 'float64',
+            z: 'float64'
+        },
+        rotation: {
+            x: 'float64',
+            y: 'float64',
+            z: 'float64',
+            w: 'float64'
+        }
+    }
+}]});
+var playerSchemad = sp.build({p: [{
+    id: 'varuint',
+    transform: {
+        position: {
+            x: 'string',
+            y: 'float64',
+            z: 'float64'
+        },
+        rotation: {
+            x: 'float64',
+            y: 'float64',
+            z: 'float64',
+            w: 'float64'
+        }
+    }
+}, {
+    id: 'varuint',
+    transform: {
+        position: {
+            x: 'float64',
+            y: 'float64',
+            z: 'float64'
+        },
+        rotation: {
+            x: 'float64',
+            y: 'float64',
+            z: 'float64',
+            w: 'float64'
+        }
+    }
+}]});
+
+var player = {p: [{
+    id: 0,
+    transform: {
+        position: {
+            x: 2,
+            y: 2.0000000004989023,
+            z: -24.90756910131313
+        },
+        rotation: {
+            x: 0.32514392007855847,
+            y: -0.8798439564294107,
+            z: 0.32514392007855847,
+            w: 0.12015604357058937
+        }
+    }
+}, {
+    id: 1,
+    transform: {
+        position: {
+            x: 7.490254936274141,
+            y: 2.0000000004989023,
+            z: -14.188117316225544
+        },
+        rotation: {
+            x: 0,
+            y: 0.018308020720336753,
+            z: 0.1830802072033675,
+            w: 0.9829274917854702
+        }
+    }
+}]};
+
+var playerd = {p: [{
+    id: 0,
+    transform: {
+        position: {
+            x: 'asdf',
+            y: 2.0000000004989023,
+            z: -24.90756910131313
+        },
+        rotation: {
+            x: 0.32514392007855847,
+            y: -0.8798439564294107,
+            z: 0.32514392007855847,
+            w: 0.12015604357058937
+        }
+    }
+}, {
+    id: 1,
+    transform: {
+        position: {
+            x: 7.490254936274141,
+            y: 2.0000000004989023,
+            z: -14.188117316225544
+        },
+        rotation: {
+            x: 0,
+            y: 0.018308020720336753,
+            z: 0.1830802072033675,
+            w: 0.9829274917854702
+        }
+    }
+}]};
+
+try{
+    var buffer = playerSchemad.encode(playerd);
+    var str = buffer;
+    console.log(str + ": " + str.length + " characters, " + Buffer.byteLength(str, 'utf8') + " bytes");
+
+}
+catch(err){
+
+}
+
+try{
+    var lskjdf = playerSchema.decode(buffer);
+}catch(err){
+    console.log(err);
+}
+console.log('asdf');
+
+str = JSON.stringify(player);
+console.log(str + ": " + str.length + " characters, " + Buffer.byteLength(str, 'utf8') + " bytes");
+
+
+
--- bin/ws.js
+++ bin/ws.js
@@ -3,13 +3,15 @@
 global.__DEV = true;
 
 var cluster = require('cluster'),
-    port = 7070;
+    nodesPerCore = 1,
+    port = 7000;
 
 if (cluster.isMaster) {
+    var masterPort = parseInt(port) + 777;
     var workers = [];
 
     // Create workers
-    for (var i=0; i<require('os').cpus().length * 2; i++) {
+    for (var i=0; i < nodesPerCore * require('os').cpus().length; i++) {
         workers[i] = cluster.fork({WORKER_INDEX:i});
     }
 
@@ -17,21 +19,21 @@ if (cluster.isMaster) {
     var netServer = require('net').createServer({pauseOnConnect:true}, function(c) {
         var r = Math.floor( Math.random()*workers.length );
         workers[r].send("doit",c);
-    }).listen(7777);
+    }).listen(masterPort);
 
     netServer.on('connect',((e) => {
         console.log(e);
     }));
 } else {
-    port = parseInt((process.env.WORKER_INDEX > 9 ? 70 : 700) + process.env.WORKER_INDEX);
-    console.log("Starting worker " + process.env.WORKER_INDEX);
+    var WORKER_INDEX = process.env.WORKER_INDEX,
+        gamePort = parseInt(port) + parseInt(WORKER_INDEX);
 
     var http = require('http'),
         WebSocketServer = require('ws').Server,
         express = require('express');
 
     var server = http.createServer(),
-        wss = new WebSocketServer({ server: server }),
+        wss = new WebSocketServer({server: server}),
         app = express();
 
     app.use(function (req, res) {
@@ -40,130 +42,145 @@ if (cluster.isMaster) {
     });
 
     wss.on('connection', function connection(ws) {
-        ws.sendObj = function(obj){
+        ws.sendObj = function (obj) {
             ws.send(JSON.stringify(obj));
 
-            if(__DEV)
+            if (__DEV)
                 console.log(Date.now() + '<-out:', JSON.stringify(obj));
         };
-        ws.on('message', function incoming(data){
-            var d = JSON.parse(data);
+        ws.on('message', function incoming(data) {
+            try{
+                console.log(typeof data);
+                var d = JSON.parse(data);
 
-            if(d.m == 'load'){
-                Game.artificioalLoad = parseInt(d.v);
-                ws.sendObj({load: Game.artificioalLoad});
+                if (d.m == 'load') {
+                    Game.artificioalLoad = parseInt(d.v);
+                    ws.sendObj({load: Game.artificioalLoad});
+                }
+
+                if (d.m == 'get') {
+                    ws.sendObj(Game.getServerLoad(true));
+                }
+
+                if (d.m == 'ping') {
+                    ws.sendObj(d);
+                }
+
+                if (d.m == 'server') {
+                    ws.sendObj({v: WORKER_INDEX});
+                }
+                if (__DEV)
+                    console.log(Date.now() + '->in:', data);
+
+
+
             }
-
-            if(d.m == 'get'){
-                ws.sendObj(Game.getServerLoad(true));
+            catch(err){
+                if (__DEV)console.log('Bad Packet: ', data);
             }
-
-            if(d.m == 'ping'){
-                ws.sendObj(d);
-            }
-
-            if(d.m == 'server'){
-                ws.sendObj({v: process.env.WORKER_INDEX});
-            }
-
-            if(__DEV)
-                console.log(Date.now() + '->in:', data);
         });
 
-        // Request login status from every new connection
         ws.sendObj({m: 'hi'});
     });
 
     server.on('request', app);
-    server.listen(port, function () { console.log('Listening on ' + server.address().port) });
+    server.listen(gamePort, function () {
+        console.log('Worker ' + WORKER_INDEX + ' listening on ' + server.address().port)
+    });
 
     // Get message from master and check if need pass to http server
-    process.on('message', function(m,c) {
-        if ( "doit" === m ) {
+    process.on('message', function (m, c) {
+        if ("doit" === m) {
             server.emit('connection', c);
             c.resume();
         }
     });
-}
-
-
-
-
 
 
 // Game Loop
-class Game {
-    static init(){
-        // Tweakable
-        this.loopDelay = 50;//20 ticks per second
+    class Game {
+        static init() {
+            // Tweakable
+            this.loopDelay = 50;//20 ticks per second
 
-        // Default
-        this.lastLoop = Date.now();
-        this.lastSecond = Date.now();
-        this.loopCount = 0;
-        this.computeCountSec = 0;
-
-
-        // Server load
-        this.serverLoad = {};
-        this.serverLoad.tick = [];
-        this.serverLoad.tps = 0; //ticks per second
-        this.serverLoad.lastSecond = Date.now();
-        this.serverLoad.current = 0;
-        this.serverLoad.high = 0;
-        this.serverLoad.low = 0;
-
-        this.artificioalLoad = 25000000;
-
-        // Start the game loop
-        this.loop();
-    }
-
-    static getServerLoad(JSON){
-        if(JSON)
-            return {'t': this.serverLoad.tps, 'a': this.serverLoad.current, 'h': this.serverLoad.high, 'l': this.serverLoad.low};
-        else
-            return 'tps: ' + this.serverLoad.tps + ' Average: ' + this.serverLoad.current + '% High: ' + this.serverLoad.high + '% Low: ' + this.serverLoad.low + '% (percent of compute time used per tick)';
-    }
-
-    static loop(){
-        setTimeout(()=>{setTimeout(()=>{this.loop()}, 1);}, this.loopDelay);
-        this.lastLoop = Date.now();
-        this.loopCount++;
-
-        // Executed once per real second
-        if(Date.now() - this.lastSecond > 1000){
+            // Default
+            this.lastLoop = Date.now();
             this.lastSecond = Date.now();
-        }
-
-        // Artificial load
-        var i = 0;
-        while(i<this.artificioalLoad){
-            i++
-        }
+            this.loopCount = 0;
+            this.computeCountSec = 0;
 
 
-        // === Server load
-        var computeTime = Date.now() - this.lastLoop;
-        var loadPercent = Math.round((100 / this.loopDelay) * computeTime);
-        this.serverLoad.tick.push(loadPercent);
-        // Executed once per game second
-        if(this.loopCount % (1000 / this.loopDelay) == 0){
-            this.serverLoad.current = this.serverLoad.tick.reduce((a, b) => a + b, 0) / this.serverLoad.tick.length;
-            this.serverLoad.low = this.serverLoad.tick.reduce((a, b) => a < b ? a:b);
-            this.serverLoad.high = this.serverLoad.tick.reduce((a, b) => a > b ? a:b);
+            // Server load
+            this.serverLoad = {};
             this.serverLoad.tick = [];
-
-
-            // Ticks per second
-            var timeLastSec = Date.now() - this.serverLoad.lastSecond;
+            this.serverLoad.tps = 0; //ticks per second
             this.serverLoad.lastSecond = Date.now();
-            this.serverLoad.tps = (1000 / timeLastSec) * (1000 / this.loopDelay);
+            this.serverLoad.current = 0;
+            this.serverLoad.high = 0;
+            this.serverLoad.low = 0;
 
-            //console.log(this.getServerLoad(false));
+            this.artificioalLoad = 200000;
+
+            // Start the game loop
+            this.loop();
         }
 
+        static getServerLoad(JSON) {
+            if (JSON)
+                return {
+                    't': this.serverLoad.tps,
+                    'a': this.serverLoad.current,
+                    'h': this.serverLoad.high,
+                    'l': this.serverLoad.low
+                };
+            else
+                return 'tps: ' + this.serverLoad.tps + ' Average: ' + this.serverLoad.current + '% High: ' + this.serverLoad.high + '% Low: ' + this.serverLoad.low + '% (percent of compute time used per tick)';
+        }
 
+        static loop() {
+            setTimeout(()=> {
+                setTimeout(()=> {
+                    this.loop()
+                }, 1);
+            }, this.loopDelay);
+            this.lastLoop = Date.now();
+            this.loopCount++;
+
+            // Executed once per real second
+            if (Date.now() - this.lastSecond > 1000) {
+                this.lastSecond = Date.now();
+            }
+
+            // Artificial load
+            var i = 0;
+            while (i < this.artificioalLoad) {
+                i++
+            }
+
+
+            // === Server load
+            var computeTime = Date.now() - this.lastLoop;
+            var loadPercent = Math.round((100 / this.loopDelay) * computeTime);
+            this.serverLoad.tick.push(loadPercent);
+            // Executed once per game second
+            if (this.loopCount % (1000 / this.loopDelay) == 0) {
+                this.serverLoad.current = this.serverLoad.tick.reduce((a, b) => a + b, 0) / this.serverLoad.tick.length;
+                this.serverLoad.low = this.serverLoad.tick.reduce((a, b) => a < b ? a : b);
+                this.serverLoad.high = this.serverLoad.tick.reduce((a, b) => a > b ? a : b);
+                this.serverLoad.tick = [];
+
+
+                // Ticks per second
+                var timeLastSec = Date.now() - this.serverLoad.lastSecond;
+                this.serverLoad.lastSecond = Date.now();
+                this.serverLoad.tps = (1000 / timeLastSec) * (1000 / this.loopDelay);
+
+                //console.log(this.getServerLoad(false));
+            }
+
+
+        }
     }
+    Game.init();
+
 }
-Game.init();
