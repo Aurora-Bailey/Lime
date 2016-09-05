c364ff7c2aaa10fb80bb8e43223c29569d43800e
Brad Thompson <bradleyglind@gmail.com>
2016-09-05 09:23:23 -0600

    getting ready to go live

--- public/es6/globalvar.js
+++ public/es6/globalvar.js
@@ -7,5 +7,5 @@ var settings = {};
 if(typeof localStorage.settings !== 'undefined')
     settings = JSON.parse(localStorage.settings);
 
-var serverList = ['192.168.0.3:7777', '173.255.224.138'];
+var serverList = ['45.79.85.50', '173.255.224.138', '173.255.205.190', '139.162.169.244', '139.162.56.151'];
 var serverActive = {};
